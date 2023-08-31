6f5793fc3e0cd3166bf1981d9867fef6f099596c
Qavi <qavi.shaikh11@gmail.com>
2023-08-31 19:44:51 +0500

    Mark as complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,10 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const Todo = ({task}) => {
+const Todo = ({task, toggleComplete}) => {
   return (
     <div className='Todo'>
-       <p>{task.task}</p>
+       <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
        <div>
           <FontAwesomeIcon icon={faPenToSquare} />
           <FontAwesomeIcon icon={faTrash} />
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -12,12 +12,15 @@ const TodoWrapper = () => {
 
       console.log(todos);
     }
+    const toggleComplete = id => {
+      setTodos(todos.map (todo => todo.id === id ? {...todo, completed: !todo.completed} : todo))
+    }
   return (
     <div className='TodoWrapper'>
       <h1>To do List</h1>
       <TodoForm addTodo = {addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} />
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} />
       ))}
      
     </div>
